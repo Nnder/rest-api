a73805dda6e172b26fd21e2e7b7bd15f4f6e4d0f
Nnder <asfahaikrkt6k56@yandex.ru>
2022-06-11 19:27:02 +0500

    mongo

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,8 +1,38 @@
+import mongoose from 'mongoose';
+
+const MONGO_USERNAME = 'nnder';
+const MONGO_PASSWORD = 'passwd';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'app';
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+mongoose.connect(url, {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    name: {type: String, required: true},
+    phone: {type: Number, required: true}
+})
+
+mongoose.model('user', User)
 
 
 class Controller{
-    async create(){
+    async createOne(){
+        let user = new User(req.body)
+        user.save((err)=>{
+            if(err){
+                res.status(500).send('Error');
+            } else {
+                res.status(201).json(user);
+            }
+        })
+    }
 
+    async createMany(){
+        
     }
 
     async getAll(){
--- Router/Router.js
+++ Router/Router.js
@@ -3,10 +3,10 @@ import controller from "../Controller/Controller.js";
 const router = new Router();
 
 
-router.get('/users');
-router.get('/users/:id');
-router.post('/users', controller.create);
-router.put('/users');
-router.delete('users/:id');
+router.get('/users', controller.getAll);
+router.get('/users/:id', controller.getOne);
+router.post('/users', controller.createOne);
+router.put('/users', controller.update);
+router.delete('users/:id', controller.delete);
 
 export default router;
