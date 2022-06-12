037a31e831e1e3174ff86ccee6c5d02e3babe3db
Nnder <asfahaikrkt6k56@yandex.ru>
2022-06-13 04:50:06 +0500

    readme

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,29 +1,25 @@
-import mongoose from 'mongoose';
 import User from '../Schema/User.js';
 
 class Controller{
     async createOne(req,res){
-        const {name, phone} = req.body;
-        console.log(req.body);
-        let result = await User.create({name,phone})
-        res.status(201).json(result);
+        try {
+            const {name, phone} = req.body;
+            let result = await User.create({name,phone})
+            res.status(201).json(result);
+        } catch (error) {
+            res.status(500).json({Message:"Error", ...e});
+        }
     }
 
     async createMany(req,res){
-
         try {
             let users = req.body;
-
-            console.log(typeof users);
             for(let user of users){
                 console.log(user);
                 const {name, phone} = user;
                 let result = await User.create({name,phone})
-            
             }
-
             res.status(201).json(users);
-
         } catch (e) {
             res.status(500).json({Message:"Error", ...e});
         }
@@ -31,26 +27,39 @@ class Controller{
     }
 
     async getAll(req,res){
-        let result = await User.find();
-        res.json(result);
+        try {
+            let result = await User.find();
+            res.json(result);
+        } catch (error) {
+            res.status(500).json({Message:"Error", ...e});
+        }
     }
 
     async getOne(req,res){
-        console.log(req.params.id);
-        let result = await User.findById(req.params.id);
-        res.json(result);
+        try {
+            let result = await User.findById(req.params.id);
+            res.json(result);
+        } catch (error) {
+            res.status(500).json({Message:"Error", ...e});
+        }
     }
 
     async delete(req,res){
-        console.log(req.params.id);
-        let result = await User.findByIdAndDelete(req.params.id);
-        res.json(result);
+        try {
+            let result = await User.findByIdAndDelete(req.params.id);
+            res.json(result);
+        } catch (error) {
+            res.status(500).json({Message:"Error", ...e});
+        }
     }
 
     async update(req,res){
-        console.log(req.params.id);
-        let result = await User.findByIdAndUpdate(req.params.id,req,body,{new:true});
-        res.json(result);
+        try {
+            let result = await User.findByIdAndUpdate(req.params.id,req,body,{new:true});
+            res.json(result);
+        } catch (error) {
+            res.status(500).json({Message:"Error", ...e});
+        }
     }
 
 
--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # rest-api
+## CRUD методы
+#### Метод POST получает нескольк JSON элементов
+### Example http://localhost:5000/api/users
+
+    [
+        {
+            "name": "John1",
+            "phone": 1234567
+        },
+        {
+            "name": "John2",
+            "phone": 124563732
+        },
+        {
+            "name": "John3",
+            "phone": 124557778
+    ]
+<hr>
+
+#### Метод GET вернет всех
+### Example http://localhost:5000/api/users
+
+    [
+    {
+        "_id": "62a67ae9c578b73baf3354e9",
+        "name": "John1",
+        "phone": 124567,
+        "__v": 0
+    },
+    {
+        "_id": "62a67aeac578b73baf3354f3",
+        "name": "John2",
+        "phone": 124563732,
+        "__v": 0
+    }
+]
+<hr>
+
+#### Метод GET по id
+### Example http://localhost:5000/api/users/:id
+
+    [
+        {
+            "_id": "62a67ae9c578b73baf3354e9",
+            "name": "John1",
+            "phone": 124567,
+            "__v": 0
+        }
+    ]
+<hr>
+
+#### Метод DELETE по id
+### Example http://localhost:5000/api/users/:id
+
+    [
+        {
+            "_id": "62a67ae9c578b73baf3354e9",
+            "name": "John1",
+            "phone": 124567,
+            "__v": 0
+        }
+    ]
+<hr>
+
+#### Метод UPDATE по id
+### Example http://localhost:5000/api/users/:id
+
+    [
+        {
+            "_id": "62a67ae9c578b73baf3354e9",
+            "name": "John1",
+            "phone": 124567,
+            "__v": 0
+        }
+    ]
+<hr>
